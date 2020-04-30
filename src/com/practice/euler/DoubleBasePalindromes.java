e127e73a0591600ef4516cc42120d7e15785077e
Srisaikrishnan V <krishnansai99@gmail.com>
2020-04-30 23:12:16 +0530

    Double Base Palindromes

--- src/com/practice/euler/DoubleBasePalindromes.java
+++ src/com/practice/euler/DoubleBasePalindromes.java
@@ -11,37 +11,32 @@ import java.math.BigInteger;
  */
 public class DoubleBasePalindromes {
 
-	public static BigInteger checkDoubleBasePalindromes(int decimal) {
+	public static int checkDoubleBasePalindromes(int decimal) {
 		int counter = 0;
 		int remainder = 0;
 		String value = null;
 		BigInteger binary = new BigInteger("0");
-		while(decimal>0) {
-			remainder = decimal%2;
-			value = binary.toString() + new BigInteger(""+remainder).toString();
-			binary = new BigInteger(""+value);
-			decimal = decimal/2;
+		for(int i=2;i<=decimal;i++) {
+			while(decimal>1) {
+				remainder = decimal%2;
+				value = binary.toString() + new BigInteger(""+remainder).toString();
+				binary = new BigInteger(""+value);
+				decimal = decimal/2;
+			}
+			if(checkPalindrome(binary)) {
+				counter++;
+			}
 		}
-		if(checkPalindrome(binary)) {
-			counter++;
-		}
-		System.out.println(counter);
-		return binary;
+		return counter;
 	}
 	
 	public static boolean checkPalindrome(BigInteger number) {
 		boolean flag = false;
-		BigInteger remainder =new BigInteger("0");
-		BigInteger reverse = new BigInteger("0");
 		BigInteger oldValue = new BigInteger(""+number);
-		int result = number.compareTo(new BigInteger("0"));
-		while(result>0) {
-			remainder = number.divide(new BigInteger("10"));
-			reverse = reverse.add(remainder);
-			number = number.remainder(new BigInteger("10"));
-			result = number.compareTo(new BigInteger("0"));
-		}
-		if(oldValue==reverse) {
+		StringBuffer revNumber = new StringBuffer(number.toString());
+		revNumber = revNumber.reverse();
+		BigInteger num = new BigInteger(""+revNumber);
+		if(oldValue.equals(num)) {
 			 flag = true;
 		}
 		return flag;
@@ -52,7 +47,7 @@ public class DoubleBasePalindromes {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(checkDoubleBasePalindromes(585));
+		System.out.println(checkDoubleBasePalindromes(1000000));
 	}
 
 }
