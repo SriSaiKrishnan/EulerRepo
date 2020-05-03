e64d4c0d0b6aa598f13640ab8e4136c848935377
Srisaikrishnan V <krishnansai99@gmail.com>
2020-05-03 23:31:08 +0530

    Digit Factorial

--- src/com/practice/euler/DigitFactorial.java
+++ src/com/practice/euler/DigitFactorial.java
@@ -9,48 +9,51 @@ package com.practice.euler;
  */
 
 /*
-145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-
-Find the sum of all numbers which are equal to the sum of the factorial of their digits.
-
-Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+ * 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+ * 
+ * Find the sum of all numbers which are equal to the sum of the factorial of
+ * their digits.
+ * 
+ * Note: as 1! = 1 and 2! = 2 are not sums they are not included.
  */
 
 public class DigitFactorial {
-	
-	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=3;i<=1000000;i++) {
-			digitFactorial(i);
-		}
+
+		System.out.println(digitFactorial(1000000));
+
 	}
 
-	private static void digitFactorial(int i) {
+	private static int digitFactorial(int num) {
 		// TODO Auto-generated method stub
-		int remainder = 0;
-		int sum = 0;
-		int oldvalue = i;
-		while(i>0) {
-			remainder = i%10;
-			sum = sum + factorial(remainder);
-			i=i/10;
+		int total = 0;
+		for (int i = 3; i <= num; i++) {
+			int remainder = 0;
+			int sum = 0;
+			int oldvalue = i;
+			int number = i;
+			while (number > 0) {
+				remainder = number % 10;
+				sum = sum + factorial(remainder);
+				number = number / 10;
+			}
+			if (oldvalue == sum) {
+				total += sum;
+			}
 		}
-		if(oldvalue == sum) {
-			System.out.println(sum + " is Digit Power");
-		}
-		
+		return total;
 	}
 
 	private static int factorial(int remainder) {
 		// TODO Auto-generated method stub
 		int fact = 1;
-		for(int i=1;i<=remainder;i++) {
-			fact = fact*i;
+		for (int i = 1; i <= remainder; i++) {
+			fact = fact * i;
 		}
 		return fact;
 	}
