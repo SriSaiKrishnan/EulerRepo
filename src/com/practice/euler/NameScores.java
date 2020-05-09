be274cd895c7c2d116ad1e263767b928c2aef1ef
Srisaikrishnan V <krishnansai99@gmail.com>
2020-05-09 11:12:13 +0530

    Name Scores

--- src/com/practice/euler/NameScores.java
+++ src/com/practice/euler/NameScores.java
@@ -45,7 +45,6 @@ public class NameScores {
 			counter ++;
 		}
 		System.out.println(nameScore);
-		System.out.println(counter);
 	}
 	
 	private static int calculateScore(String name) {
@@ -58,87 +57,7 @@ public class NameScores {
 	}
 	
 	private static int charValue(char ch) {
-		int n = 0;
-		switch(ch) {
-		case 'A':
-			n = 1;
-			break;
-		case 'B':
-			n = 2;
-			break;
-		case 'C':
-			n = 3;
-			break;
-		case 'D':
-			n = 4;
-			break;
-		case 'E':
-			n = 5;
-			break;
-		case 'F':
-			n = 6;
-			break;
-		case 'G':
-			n = 7;
-			break;
-		case 'H':
-			n = 8;
-			break;
-		case 'I':
-			n = 9;
-			break;
-		case 'J':
-			n = 10;
-			break;
-		case 'K':
-			n = 11;
-			break;
-		case 'L':
-			n = 12;
-			break;
-		case 'M':
-			n = 13;
-			break;
-		case 'N':
-			n = 14;
-			break;
-		case 'O':
-			n = 15;
-			break;
-		case 'P':
-			n = 16;
-			break;
-		case 'Q':
-			n = 17;
-			break;
-		case 'R':
-			n = 18;
-			break;
-		case 'S':
-			n = 19;
-			break;
-		case 'T':
-			n = 20;
-			break;
-		case 'U':
-			n = 21;
-			break;
-		case 'V':
-			n = 22;
-			break;
-		case 'W':
-			n = 23;
-			break;
-		case 'X':
-			n = 24;
-			break;
-		case 'Y':
-			n = 25;
-			break;
-		case 'Z':
-			n = 26;
-			break;
-		}
+		int n = ((int)ch) - 64;
 		return n;
 	}
 	
